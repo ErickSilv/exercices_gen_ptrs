21555e2de61d65d73e6b1c5e9368497cd2b02b45
ErickSilv <erickoliveira.eos@gmail.com>
2017-04-10 15:30:54 -0300

    All 5 questions conclued

--- find_if.cpp
+++ find_if.cpp
@@ -2,30 +2,42 @@
 #include <cstring>
 using byte = unsigned char;
 
-
+//Test function (predicates)
 void print ( int *first, int *last ) {
 
 	std::cout << " = [ ";
-	while (first != last){
+	while ( first != last ){
 		std::cout << *first << " ";
 		first++;
 	}
 	std::cout << "] \n";
+
 }
 
-
-bool menor ( const void *a, const void *b ) {
+bool positivo ( const void *a ) {
 	
-	auto num1 = (int * ) a;
-	auto num2 = (int * ) b;
-
-	if ( *num1 < *num2 ) {
+	auto num1 = ( int * ) a;
+	if ( *num1 <= 0 ) {
 		return true;
 	}
 	return false;
+
+}
+
+bool igual ( const void *a, const void *b ) {
+	
+	auto num1 = ( int * ) a;
+	auto num2 = ( int * ) b;
+	if ( *num1 == *num2 ) {
+		return true;
+	}
+	return false;
+
 }
 
 
+
+// Generic Functions with generic pointers
 using UnaryPredicate = bool (*) ( const void * );
 void * Find_If ( const void * first_, const void * last_, UnaryPredicate p , size_t size ){
 
@@ -55,7 +67,7 @@ void * Min ( const void * first_ , const void * last_ , Compare cmp , size_t siz
 	auto min = first;
 	for ( /*Empty*/; first != last; first += size) {
 		
-		if ( menor ( first, min ) ) {
+		if ( cmp ( first, min ) ) {
 			min = first;
 		}
 
@@ -81,19 +93,83 @@ void * Copy ( const void * first_ , const void * last_ , const void * c_first_ ,
 	return c_first;
 }
 
+using Condition = bool (*)( const void * );
+void * Remove_If ( const void * first_ , const void * last_ , Condition con , size_t size ) {
 
+	auto first = (byte *) first_;
+	auto last  = (byte *) last_;
 
+	for ( /*Empty*/; first != last; first += size ) {
+
+		if ( first == (last-size) && con(first) ) {
+			last -= size;
+		} else {
+			while ( con(first) ) { 
+				auto aux = first;
+				for ( /*Empty*/ ; aux != ( last-size ); aux += size ) {
+					std::memmove( aux , ( aux+size ), size );
+				}
+				last -= size;
+								
+			}
+		}
+	}
+	
+	return last;
+}
+
+using Igualdade = bool (*)( const void *, const void * );
+void * Unique ( const void * first_ , const void * last_ , Igualdade igul , size_t size ) {
+
+	auto first = (byte *) first_;
+	auto last  = (byte *) last_;
+
+	auto iSlow = first+size;
+	auto iFast = first+size;
+
+	for ( /*Empty*/; first != last; first += size) {
+
+		auto compar = false;
+		auto aux = (byte *) first_; 
+		for ( /*Empty*/; aux != iSlow; aux += size ) {
+			compar = igul ( aux, iFast );
+
+			if ( compar ) {
+
+				break;
+
+			}
+
+		}
+
+		//If anyone comparation returns true so move the value at address referencied by pointer iFast to a 
+		//	address referencied by pointer iSlow.
+		if ( !compar ) {
+			
+			//Move the value to a address.
+			std::memmove( iSlow, iFast, size );
+			// 'Walk' one -size- of bytes if iFast refer to a unique value.
+			iSlow += size;
+
+		}
+
+		iFast += size;
+
+	}
+
+	//Returning (iSlow-size) because the pointer 'iSlow' is a reference to value what is memory garbage  
+	return iSlow-size;
+
+}
 
 
 int main () {
 
-	int A[11] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1 };
-	int B[11];
+	int A[11] = { -1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11 };
+	//int B[10] = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+	auto ptr_ = (int *) Remove_If( std::begin(A), std::end(A), positivo, sizeof(int) );
 
-	auto ptr_ = (int *) Copy(std::begin(A), std::end(A), std::begin(B), sizeof(int) );
-
-
-	std::cout << " B";
-	print( std::begin(B), ptr_ );
+	std::cout << " A ";
+	print( std::begin(A), ptr_ );
 
 }
