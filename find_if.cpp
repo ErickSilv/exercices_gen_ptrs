abbfc654e71614ce790e0be6e59ba11b43c9e5f4
ErickSilv <erickoliveira.eos@gmail.com>
2017-04-10 22:53:23 -0300

    Q6 and Q7 of a aval1 2016.1 LP resolved

--- find_if.cpp
+++ find_if.cpp
@@ -1,40 +1,71 @@
-#include <iostream>
-#include <cstring>
+#include <iostream> //cout, cin, endl
+#include <iomanip> //setW
+#include <cstring> 
+#include <iterator> // begin, end
 using byte = unsigned char;
 
-//Test function (predicates)
-void print ( int *first, int *last ) {
+typedef struct Student {
+	std::string name;
+	float grades[3];
+	int year;
+} Student;
 
-	std::cout << " = [ ";
+//Test function (predicates)
+void printAllStudents ( Student *first, Student *last ) {
+
+	std::cout << "Students = [\n";
 	while ( first != last ){
-		std::cout << *first << " ";
+		std::cout << "{ ";
+		std::cout << std::setw(13) << std::left << first->name << ", {";
+		for ( auto i=0; i < 3; ++i ) {
+			std::cout << std::setw(3) << first->grades[i];
+		}
+		std::cout << "}, Y=" << std::setw(5) << first->year << " }," << std::endl;
 		first++;
 	}
+	std::cout << std::setw (42) << std::right << " ]\n";
+}
+
+void printOneStudent ( Student * est ) {
+
+	std::cout << "Student = [ ";
+		std::cout << est->name << ", { ";
+		for ( auto i=0; i < 3; ++i ) {
+			std::cout << est->grades[i] << " ";
+		}
+		std::cout << "}, Y=" << est->year;
 	std::cout << "] \n";
-
 }
 
-bool positivo ( const void *a ) {
+bool menor_media ( const void * a_, const void *b_ ) {
+
+	auto a = (Student *) a_;
+	auto b = (Student *) b_;
+
+	float media_a=0;
+	float media_b=0;
+	for ( auto i=0; i < 3; ++i ) {
+		media_a += a->grades[i];
+		media_b += b->grades[i];
+	}
+
+	if ( media_a/3 < media_b/3 ) {
+		return true;
+	}
+
+	return false;
+}
+
+bool comparByName_Student ( const void *a_, const void *b_ ) {
+
+	auto a = (Student *) a_;
+	auto b = (Student *) b_;
 	
-	auto num1 = ( int * ) a;
-	if ( *num1 >= 0 ) {
+	if ( a->name == b->name ) {
 		return true;
 	}
 	return false;
-
 }
-
-bool igual ( const void *a, const void *b ) {
-	
-	auto num1 = ( int * ) a;
-	auto num2 = ( int * ) b;
-	if ( *num1 == *num2 ) {
-		return true;
-	}
-	return false;
-
-}
-
 
 
 // Generic Functions with generic pointers
@@ -66,7 +97,7 @@ void * Min ( const void * first_ , const void * last_ , Compare cmp , size_t siz
 
 	auto min = first;
 	for ( /*Empty*/; first != last; first += size) {
-		
+		//IF min < first
 		if ( cmp ( first, min ) ) {
 			min = first;
 		}
@@ -157,11 +188,30 @@ void * Unique ( const void * first_ , const void * last_ , Igualdade igul , size
 
 int main () {
 
-	int A[13] = { -1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, -1, -9};
-	//int B[10] = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
-	auto ptr_ = (int *) Unique( std::begin(A), std::end(A), igual, sizeof(int) );
+	Student arr[] = { { "Leonidas" ,  7.5, 3.2, 6.7, 20142 },
+                      { "Musashi"  ,  9.5, 8.2, 8.7, 20131 },
+                      { "Spartacus",  2.1, 3.5, 5.5, 20123 },
+                      { "Miltiades",  0.1, 5.6, 4.4, 20151 },
+                      { "Aquiles"  ,  6.5, 7.1, 8.6, 20141 },
+                      { "Thor"     ,  4.5, 6.2, 2.1, 20132 },
+                      { "Lancelot" ,  6.5, 6.0, 7.4, 20122 },
+                      { "Ragnar"   ,  2.1, 6.6, 4.7, 20131 },
+                      { "Ragnar"   ,  1.1, 6.6, 4.7, 20131 }
+    };
 
-	std::cout << " A ";
-	print( std::begin(A), ptr_ );
+    //auto arrSz = sizeof(arr)/sizeof(Student);
 
+    Student c_arr[9];
+
+
+
+    auto ptr_ = (Student *) Copy ( std::begin(arr), std::end(arr), std::begin(c_arr), sizeof(Student) );
+
+    printAllStudents ( std::begin(c_arr), ptr_ );
+    
+    ptr_ = (Student *) Unique( std::begin(c_arr), std::end(c_arr), comparByName_Student, sizeof(Student) );
+
+	printAllStudents ( std::begin(c_arr), ptr_ );
+
+	
 }
